24ae2020cfecd01962bae9addc2ae40b795bc764
Oleksii Smirnov <o.smirnov@e-tender.ua>
2021-04-26 16:22:24 +0300

    Why so difficult to make a good commit message?

--- fate.js
+++ fate.js
@@ -1,129 +1,56 @@
+/**
+ *  "Technically speaking, your URL should never be longer than 2,048 characters"
+ *  So, according to this calculation
+ *  2000 / '/ph582c6a6a90147'.length // 125
+ *  up to 125 video-keys could be passed by URL. UP TO 125!!!
+ */
+
 // Init
 const mainEl = document.getElementById('main');
 
 const SIX_SECONDS = 6666;
-const FEW_MINUTES = 0xA * SIX_SECONDS * ( 1 + ~~(Math.random()*0xF));
-
-// Может лучше
-
+const FEW_MINUTES = 0xA * SIX_SECONDS * (1 + ~~(Math.random() * 0xF));
+const defaultViewKeys = [
+    "ph582c6a6a90147",
+    "pm554ceda34a835",
+    "ph5e01c31d04538",
+    "ph5d1fac50cd0e3",
+    "ph5ee793cda1428",
+    "ph5edc0dd547142",
+    "ph5ae74628cec94",
+    "ph5f21700d2d46e",
+    "ph5edd6abac6c6c",
+    "ph5ad59f0f5e4f2",
+    "ph5a7d7c1145969",
+    "ph5e85ab166e122",
+    "ph6023e713677c6",
+    "ph5f3858cd0892e",
+    "ph5cc555b520cc5",
+    "ph60423eb8a7397",
+    "ph59faf120a2db4",
+    "ph6040556055c5c",
+    "ph5fe40e45d725f",
+    "ph5f1ea54524386",
+    "ph604260a74779c",
+    "ph59d143400aae5",
+    "ph5f6db156504f8",
+    "ph605cbeb4cdafa",
+    "ph605ce95487ce6",
+    "ph6023e713677c6",
+    "ph60695cd88d4c7"
+];
 
 //Exposition
-const someVideos = [{
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5f1ea54524386&pkey=190335392",
-    "viewkey": "ph5f1ea54524386",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5f1ea54524386",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5aeca92e6f071",
-    "viewkey": "ph5aeca92e6f071",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5aeca92e6f071",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph59d143400aae5&pkey=sys:dvd:372022",
-    "viewkey": "ph59d143400aae5",
-    "iframeUrl": "http://www.pornhub.com/embed/ph59d143400aae5",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph5f6db156504f8&t=1278&utm_source=127.0.0.1:8080&utm_medium=embed&utm_campaign=embed-title-html5",
-    "viewkey": "ph5f6db156504f8",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5f6db156504f8",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}];
-
-const nextLevelVideos = [{
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph6023e713677c6",
-    "viewkey": "ph6023e713677c6",
-    "iframeUrl": "http://www.pornhub.com/embed/ph6023e713677c6",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5f3858cd0892e&pkey=80535321",
-    "viewkey": "ph5f3858cd0892e",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5f3858cd0892e",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph5cc555b520cc5",
-    "viewkey": "ph5cc555b520cc5",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5cc555b520cc5",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph60423eb8a7397&pkey=173458352",
-    "viewkey": "ph60423eb8a7397",
-    "iframeUrl": "http://www.pornhub.com/embed/ph60423eb8a7397",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph59faf120a2db4&pkey=80535321",
-    "viewkey": "ph59faf120a2db4",
-    "iframeUrl": "http://www.pornhub.com/embed/ph59faf120a2db4",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-},{
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=40097746&pkey=166229981",
-    "viewkey": "40097746",
-    "iframeUrl": "http://www.pornhub.com/embed/40097746",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph6040556055c5c",
-    "viewkey": "ph6040556055c5c",
-    "iframeUrl": "http://www.pornhub.com/embed/ph6040556055c5c",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5fe40e45d725f&pkey=190334792",
-    "viewkey": "ph5fe40e45d725f",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5fe40e45d725f",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}];
-const ultimateLevel = [ {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph602fd12be254c",
-    "viewkey": "ph602fd12be254c",
-    "iframeUrl": "http://www.pornhub.com/embed/ph602fd12be254c",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5f602682bb8ff&pkey=80535321",
-    "viewkey": "ph5f602682bb8ff",
-    "iframeUrl": "http://www.pornhub.com/embed/ph5f602682bb8ff",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph602a9a710520e",
-    "viewkey": "ph602a9a710520e",
-    "iframeUrl": "http://www.pornhub.com/embed/ph602a9a710520e",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5e506261a4469&pkey=80535321",
-    "viewkey": "ph5e506261a4469",
-    "iframeUrl": "https://www.pornhubpremium.com/view_video.php?viewkey=ph5e506261a4469&pkey=80535321",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}];
-
-
-const iJustDoNotKnowHowToCallIt = [{
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph605cbeb4cdafa&utm_source=127.0.0.1&utm_medium=embed&utm_campaign=embed-removed-nt4x4-html5",
-    "viewkey": "ph605cbeb4cdafa",
-    "iframeUrl": "http://www.pornhub.com/embed/ph605cbeb4cdafa",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph605ce95487ce6&utm_source=127.0.0.1&utm_medium=embed&utm_campaign=embed-removed-nt4x4-html5",
-    "viewkey": "ph605ce95487ce6",
-    "iframeUrl": "http://www.pornhub.com/embed/ph605ce95487ce6",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph6023e713677c6&utm_source=127.0.0.1:8080&utm_medium=embed&utm_campaign=embed-title-html5",
-    "viewkey": "ph6023e713677c6",
-    "iframeUrl": "http://www.pornhub.com/embed/ph6023e713677c6",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}, {
-    "url": "https://www.pornhub.com/view_video.php?viewkey=ph60695cd88d4c7",
-    "viewkey": "ph60695cd88d4c7",
-    "iframeUrl": "http://www.pornhub.com/embed/ph60695cd88d4c7",
-    "iframe": {"frameborder": "0", "autoplay": true, "allowfullscreen": true}
-}];
-
-
-
-
-
-
-
-
-
+let videos = [];
+window.addEventListener('hashchange', hashChanged);
+hashChanged();
+function hashChanged() {
+    const viewKeys = location.hash.length > 1 ? (location.hash.substr(1).split('/')) : defaultViewKeys;
+    console.info('Using viewkeys:', viewKeys);
+    videos = viewKeys.map(viewKeyToVideoObject).filter(v => !!v);
+    console.info('Videos:', videos);
+    startTheShow();
+}
 
 function playVideos(videos) {
     const videosFragment = createPornHubIframe(videos.map(video => video.viewkey));
@@ -131,27 +58,32 @@ function playVideos(videos) {
     mainEl.appendChild(videosFragment);
 }
 
-setTimeout(() => {
-    // wait until start animation ends
-    //playVideos(ultimateLevel);
-    console.log('playing: iJustDoNotKnowHowToCallIt')
-    playVideos(iJustDoNotKnowHowToCallIt);
-    // @todo: add countdown to next wave (how?).
+function shuffleVideos() {
+    // @todo: must be a better way to do it.
+    videos = videos.sort(() => -(1/2) + Math.random());
+}
+
+function startTheShow() {
     setTimeout(() => {
-        // @todo: some "pulse"/"blink" animation effect?
-        console.log('playing: someVideos')
-        playVideos(someVideos);
+        // wait until start animation ends
+        const width = window.screen.availWidth;
+        const height = window.screen.availHeight;
+        const columnsCount = ~~(width / 640);
+        const rowsCount = ~~(height / 480)
+        const videosCount = columnsCount * rowsCount;
+        shuffleVideos();
+        const videosToPlay = videos.filter((v, i) => i < videosCount);
+        playVideos(videosToPlay);
 
-        setTimeout(() => {
-            console.log('playing: nextLevelVideos')
-            playVideos(nextLevelVideos);
-        }, FEW_MINUTES * 3);
+        // @todo: countdown to next wave/iteration/video-change?
+        // Like CockHero Temptation (which one?), the "Endless Road Track" I think.
+        // @todo: background music (not always, but needed)
+        //Rising action
+        //Climax
+        //Denouement
+    }, SIX_SECONDS);
+}
 
-    }, FEW_MINUTES * Math.SQRT2);
-    // @todo: countdown to next wave/iteration/video-change?
-}, SIX_SECONDS);
-// Like CockHero Temptation (which one?), the "Endless Road Track" I think.
-// @todo: background music (not always, but needed)
-//Rising action
-//Climax
-//Denouement
+startTheShow();
+
+
