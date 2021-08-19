53b3e4468179f08084a6654822b021d3a989c1c5
Oleksii Smirnov <o.smirnov@e-tender.ua>
2021-08-19 12:34:06 +0300

    🦾

--- src/app/fate/fate.component.ts
+++ src/app/fate/fate.component.ts
@@ -12,11 +12,21 @@ export class FateComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    this.sizeChanged(window.screen.availWidth, window.screen.availHeight);
   }
   // @todo: call this method when window size changed (how?)
   sizeChanged(width: number, height: number): void {
-    const columnsCount = Math.floor(width / 420);
-    const rowsCount = Math.floor(height / 260);
+    // @todo: use actual sizes!
+    const videoRect = [
+      {width: 2048, height: 1080},
+      {width: 1080, height: 1024},
+      {width: 1024, height: 769},
+      {width: 840, height: 520},
+      {width: 640, height: 480},
+      {width: 420, height: 260},
+    ].find(rect => rect.width <= (width / 2) && rect.height <= (height / 2));
+    const columnsCount = Math.floor(width / videoRect.width);
+    const rowsCount = Math.floor(height / videoRect.height);
     this.videos = Array(columnsCount * rowsCount).fill(new Video());
   }
 }
